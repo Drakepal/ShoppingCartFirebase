3366cdf5a969678cd0846ed521aff81c549e2012
Drakepal <playonplay23@gmail.com>
2023-04-17 13:57:51 -0500

    can click on a card and it adds it to your cart and the database cart, app works

--- app/src/main/java/com/example/shoppingcartfirebase/view/MainActivity.kt
+++ app/src/main/java/com/example/shoppingcartfirebase/view/MainActivity.kt
@@ -5,20 +5,45 @@ import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.shoppingcartfirebase.R
 import com.example.shoppingcartfirebase.databinding.ActivityMainBinding
+import com.example.shoppingcartfirebase.model.CartModel
 import com.example.shoppingcartfirebase.model.MenuModel
+import com.example.shoppingcartfirebase.viewmodel.CartLoadListener
 import com.example.shoppingcartfirebase.viewmodel.MenuAdapter
 import com.example.shoppingcartfirebase.viewmodel.MenuLoadListener
+import com.example.shoppingcartfirebase.viewmodel.UpdateCart
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
-class MainActivity : AppCompatActivity(), MenuLoadListener {
+class MainActivity : AppCompatActivity(), MenuLoadListener, CartLoadListener {
 
     private lateinit var binding: ActivityMainBinding
 
     lateinit var menuLoadListener: MenuLoadListener
+    lateinit var cartLoadListener: CartLoadListener
+
+    override fun onStart() {
+        super.onStart()
+        EventBus.getDefault().register(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (EventBus.getDefault().hasSubscriberForEvent(UpdateCart::class.java))
+            EventBus.getDefault().removeStickyEvent(UpdateCart::class.java)
+        EventBus.getDefault().unregister(this)
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
+
+    fun onUpdateCartEvent(event: UpdateCart) {
+        countCartFromFirebase()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +51,28 @@ class MainActivity : AppCompatActivity(), MenuLoadListener {
         setContentView(binding.root)
         init()
         loadMenuFromFirebase()
+        countCartFromFirebase()
+    }
+
+    private fun countCartFromFirebase() {
+        val cartModels: MutableList<CartModel> = ArrayList()
+        FirebaseDatabase.getInstance()
+            .getReference("Cart")
+            .child("CUSTOMER")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                   for(cartSnapshot in snapshot.children) {
+                       var cartModel = cartSnapshot.getValue(CartModel::class.java)
+                       cartModel!!.key = cartSnapshot.key
+                       cartModels.add(cartModel)
+                   }
+                    cartLoadListener.onLoadCartSuccess(cartModels)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    cartLoadListener.onLoadCartFailed(error.message)
+                }
+            })
     }
 
     private fun loadMenuFromFirebase() {
@@ -55,6 +102,7 @@ class MainActivity : AppCompatActivity(), MenuLoadListener {
 
     private fun init() {
         menuLoadListener = this
+        cartLoadListener = this
 
         val gridLayoutManager = GridLayoutManager(this, 2)
         binding.menuRecycler.layoutManager = gridLayoutManager
@@ -62,11 +110,21 @@ class MainActivity : AppCompatActivity(), MenuLoadListener {
     }
 
     override fun onMenuLoadSuccess(menuModelList: List<MenuModel>?) {
-        val adapter = MenuAdapter(this, menuModelList!!)
+        val adapter = MenuAdapter(this, menuModelList!!, cartLoadListener)
         binding.menuRecycler.adapter = adapter
     }
 
     override fun onMenuLoadFailure(message: String?) {
         Snackbar.make(binding.mainLayout, message!!, Snackbar.LENGTH_LONG).show()
+    }
+
+    override fun onLoadCartSuccess(cartModelList: List<CartModel>) {
+        var cartSum = 0
+        for(cartModel in cartModelList!!) cartSum += cartModel!!.quantity
+        binding.badge!!.setNumber(cartSum)
+    }
+
+    override fun onLoadCartFailed(message: String?) {
+//        Snackbar.make(binding.mainLayout, message!!, Snackbar.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/example/shoppingcartfirebase/viewmodel/CartLoadListener.kt
@@ -0,0 +1,9 @@
+package com.example.shoppingcartfirebase.viewmodel
+
+
+import com.example.shoppingcartfirebase.model.CartModel
+
+interface CartLoadListener {
+    fun onLoadCartSuccess(cartModelList: List<CartModel>)
+    fun onLoadCartFailed(message: String?)
+}
