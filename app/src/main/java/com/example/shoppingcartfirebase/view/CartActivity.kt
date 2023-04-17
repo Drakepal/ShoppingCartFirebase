3ed96d290cde79335eff6532b724635c048008e6
Drakepal <playonplay23@gmail.com>
2023-04-17 14:45:58 -0500

    making cart card view, app still runs and works

---
+++ app/src/main/java/com/example/shoppingcartfirebase/view/CartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoppingcartfirebase.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoppingcartfirebase.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
